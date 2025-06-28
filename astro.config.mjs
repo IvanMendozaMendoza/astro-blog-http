eeadce05f0300a30c690e20a019acfbeb088be05
ivanmendozamendoza <ivan@LAPTOP-7KJ4UF1L.localdomain>
2025-06-28 17:51:39 -0600

    updated config and pnpm lock (removing astro studio)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-
 import cloudflare from '@astrojs/cloudflare';
-
-
-import db from '@astrojs/db';
-
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   adapter: cloudflare(),
-  integrations: [db()],
+  integrations: [mdx(), sitemap()]
 });
