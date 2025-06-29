5d70cff78e89439f3d4a924d1e72c1e58ff15712
ivanmendozamendoza <ivan@LAPTOP-7KJ4UF1L.localdomain>
2025-06-28 18:01:02 -0600

    tryone

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
+import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  integrations: [sitemap(), db()],
   adapter: cloudflare(),
-  integrations: [mdx(), sitemap()]
 });
